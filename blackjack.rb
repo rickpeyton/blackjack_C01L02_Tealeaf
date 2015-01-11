44b9953e9823d6dfb495993d379d4b8531da45b9
Rick Peyton <peytorb@gmail.com>
2015-01-11 06:01:32 -0600

    working blackjack game

--- blackjack.rb
+++ blackjack.rb
@@ -45,6 +45,10 @@ module Hand
     puts "=> #{hand_total}"
   end
 
+  def get_card(from_deck)
+    cards << from_deck.deal_a_card
+  end
+
   def hand_total
     values = cards.map{ |card| card.value }
 
@@ -78,43 +82,114 @@ end
 
 class Player
   include Hand
+  include Comparable
   attr_accessor :name, :cards
   def initialize(name)
     @name = name
     @cards = []
   end
-end
 
-deck = Deck.new
-deck.shuffle_deck!
-player = Player.new('Rick')
-player.cards << deck.deal_a_card
-player.cards << deck.deal_a_card
-player.show_hand
-dealer = Dealer.new
-dealer.cards << deck.deal_a_card
-dealer.cards << deck.deal_a_card
-dealer.show_hand
+  def <=>(dealer)
+    if self.hand_total == dealer.hand_total
+      0
+    elsif (self.hand_total > dealer.hand_total) ||
+          (dealer.hand_total > 21)
+      1
+    else
+      -1
+    end
+  end
+end
 
 class Game
+  attr_accessor :deck, :player, :dealer
   def initialize
+    @deck = Deck.new
+    @deck.shuffle_deck!
+    @player = Player.new('Rick')
+    @dealer = Dealer.new
+    system 'clear'
   end
-# A dealer shuffles two decks of cards
-# A dealer deals one card face down to player
-# A dealer deals one card face down to self
-# A dealer deals one card face up to player
-# A dealer deals one card face up to self
-#
-# Player flips card
-# If player does not hit blackjack
-# Player can hit or stay
-# Player will either hit blackjack, bust or can hit or stay
-# If player busts then dealer wins
-#
-# Dealer flips card
-# If dealer does not hit blackjack
-# If dealer is less than 17 dealer must hit
-# If dealer is 17 or higher dealer must stay
-#
-# Compare score and determine winner
+
+  def play
+    deal_cards
+    play_player_hand
+    if player.hand_total > 21
+      puts 'Bust! Dealer wins'
+    else
+      play_dealer_hand
+      pick_winner
+    end
+    redeal if play_again?
+  end
+
+  def play_again?
+    puts 'Would you like to play again?'
+    true if gets.chomp.downcase == 'y'
+  end
+
+  def redeal
+    @deck = Deck.new
+    @deck.shuffle_deck!
+    player.cards = []
+    dealer.cards = []
+    system 'clear'
+    play
+  end
+
+  def pick_winner
+    if player == dealer
+      puts 'It is a push'
+    elsif player > dealer
+      puts "#{player.name} wins!"
+    else
+      puts "#{dealer.name} wins... Boo!"
+    end
+  end
+
+  def play_dealer_hand
+    dealer.show_hand
+    while dealer.hand_total < 17
+      hit(dealer)
+    end
+    puts 'Blackjack!' if blackjack?(dealer)
+    puts 'Bust!' if bust?(dealer)
+  end
+
+  def play_player_hand
+    player.show_hand
+    hit_or_stay unless blackjack?(player)
+    puts 'Blackjack!' if blackjack?(player)
+  end
+
+  def hit_or_stay
+    begin
+      puts 'Would you like to hit or stay? (h/s)'
+      response = gets.chomp.downcase
+      hit(player) if response == 'h'
+      response = 's' if bust?(player) || blackjack?(player)
+    end while response == 'h'
+  end
+
+  def bust?(a_player)
+    a_player.hand_total > 21
+  end
+
+  def blackjack?(a_player)
+    a_player.hand_total == 21
+  end
+
+  def hit(a_player)
+    a_player.get_card(deck)
+    a_player.show_hand
+  end
+
+  def deal_cards
+    2.times do
+      player.get_card(deck)
+      dealer.get_card(deck)
+    end
+  end
 end
+
+Game.new.play
